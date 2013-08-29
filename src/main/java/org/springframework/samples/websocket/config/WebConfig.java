e9a105a35237e2acd93972b5a1c2420cb49c6f35
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2013-08-28 21:17:43 -0400

    Make use of the new @EnableWebSocket Java config

--- src/main/java/org/springframework/samples/websocket/config/WebConfig.java
+++ src/main/java/org/springframework/samples/websocket/config/WebConfig.java
@@ -1,44 +1,31 @@
 package org.springframework.samples.websocket.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.samples.websocket.echo.EchoWebSocketHandler;
 import org.springframework.samples.websocket.snake.SnakeWebSocketHandler;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.server.support.WebSocketHttpRequestHandler;
-import org.springframework.web.socket.sockjs.SockJsHttpRequestHandler;
-import org.springframework.web.socket.sockjs.SockJsService;
-import org.springframework.web.socket.sockjs.transport.handler.DefaultSockJsService;
+import org.springframework.web.socket.server.config.EnableWebSocket;
+import org.springframework.web.socket.server.config.WebSocketConfigurer;
+import org.springframework.web.socket.server.config.WebSocketHandlerRegistry;
 import org.springframework.web.socket.support.PerConnectionWebSocketHandler;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+@EnableWebSocket
+public class WebConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
 
-	@Bean
-	public SimpleUrlHandlerMapping handlerMapping() {
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 
-		SockJsService sockJsService = new DefaultSockJsService(sockJsTaskScheduler());
+		registry.addHandler(echoWebSocketHandler(), "/echo");
+		registry.addHandler(snakeWebSocketHandler(), "/snake");
 
-		Map<String, Object> urlMap = new HashMap<String, Object>();
-		urlMap.put("/echo", new WebSocketHttpRequestHandler(echoWebSocketHandler()));
-		urlMap.put("/snake", new WebSocketHttpRequestHandler(snakeWebSocketHandler()));
-		urlMap.put("/sockjs/echo/**", new SockJsHttpRequestHandler(sockJsService, echoWebSocketHandler()));
-		urlMap.put("/sockjs/snake/**", new SockJsHttpRequestHandler(sockJsService, snakeWebSocketHandler()));
-
-		SimpleUrlHandlerMapping hm = new SimpleUrlHandlerMapping();
-		hm.setOrder(-1);
-		hm.setUrlMap(urlMap);
-
-		return hm;
+		registry.addHandler(echoWebSocketHandler(), "/sockjs/echo").withSockJS();
+		registry.addHandler(snakeWebSocketHandler(), "/sockjs/snake").withSockJS();
 	}
 
 	@Bean
@@ -49,13 +36,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public WebSocketHandler snakeWebSocketHandler() {
 		return new SnakeWebSocketHandler();
-	}
-
-	@Bean
-	public ThreadPoolTaskScheduler sockJsTaskScheduler() {
-		ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
-		taskScheduler.setThreadNamePrefix("SockJS-");
-		return taskScheduler;
 	}
 
 	// Allow serving HTML files through the default Servlet
