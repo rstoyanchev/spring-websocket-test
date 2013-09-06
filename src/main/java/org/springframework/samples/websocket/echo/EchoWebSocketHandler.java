7ffedf992f8b24814e76766526d659b7d49f51c4
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2013-09-06 12:23:27 -0400

    Polish EchoWebSocketHandler

--- src/main/java/org/springframework/samples/websocket/echo/EchoWebSocketHandler.java
+++ src/main/java/org/springframework/samples/websocket/echo/EchoWebSocketHandler.java
@@ -1,9 +1,6 @@
 package org.springframework.samples.websocket.echo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketSession;
@@ -14,9 +11,8 @@ import org.springframework.web.socket.adapter.TextWebSocketHandlerAdapter;
  */
 public class EchoWebSocketHandler extends TextWebSocketHandlerAdapter {
 
-	private static Logger logger = LoggerFactory.getLogger(EchoWebSocketHandler.class);
-
 	private final EchoService echoService;
+
 
 	@Autowired
 	public EchoWebSocketHandler(EchoService echoService) {
@@ -24,20 +20,9 @@ public class EchoWebSocketHandler extends TextWebSocketHandlerAdapter {
 	}
 
 	@Override
-	public void afterConnectionEstablished(WebSocketSession session) {
-		logger.debug("Opened new session in instance " + this);
-	}
-
-	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		String echoMessage = this.echoService.getMessage(message.getPayload());
-		logger.debug(echoMessage);
 		session.sendMessage(new TextMessage(echoMessage));
-	}
-
-	@Override
-	public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
-		session.close(CloseStatus.SERVER_ERROR);
 	}
 
 }
